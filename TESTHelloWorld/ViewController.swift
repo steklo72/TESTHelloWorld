c76686b060df8450951000df27130861396e8318
Aleksandr Fedotov <sasha4936@yandex.ru>
2022-09-05 00:14:23 +0500

    Initial Commit

---
+++ TESTHelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TESTHelloWorld
+//
+//  Created by Fedotov Aleksandr on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
